c431bbf2b59da77131451a0c7c1add5b57aea220
gargi9837 <75661376+gargi9837@users.noreply.github.com>
2021-07-02 22:22:38 -0700

    Add files via upload

--- stack.c
+++ stack.c
@@ -1,73 +1,93 @@
-// C program for array implementation of stack
-#include <limits.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h> 
+# define max 10
+typedef struct stack{
+    int top;
+    int ar[max];
+}stack1;
+typedef enum{false,true} boolean;
+void createstack(stack1 *sp);
+boolean isempty(stack1 *sp);
+boolean isfull(stack1 *sp);
+int pop(stack1 *sp);
+void push(stack1 *sp,int element);
+int peek(stack1 *sp);
 
-// A structure to represent a stack
-struct Stack {
-	int top;
-	unsigned capacity;
-	int* array;
-};
+void main(){
+    int choice,value,poped,item;
+   stack1  s;
+   createstack(&s);
+   while(1){
+       printf("\n1.Push");
+       printf("\n2.Pop");
+       printf("\n3.Peek");
+       printf("\n4.Exit");
+       printf("\nEnter your choice");
+       scanf("%d",&choice);
+       switch(choice)
+       {
+           case 1:
+           if(isfull(&s)){
+               printf("Stack is full\n");
+           }
+           else{
+           printf("\nEnter Element");
+           scanf("%d",&value);
+           push(&s,value);
+           }
+           break;
+           case 2:
+           if(isempty(&s)){
+               printf("\nStack is empty");
+               
+           }
+           else{
+              poped=pop(&s);
+              printf("Poped element is %d",poped);
+           }
+           break;
+           case 3:
+           if (isempty(&s)){
+               printf("\nStack Empty");
+           }
+           else{
+               item=peek(&s);
+               printf("Peeped element is %d",item);
+           }
+           break;
+           case 4:
+           exit(0);
+           default:
+           printf("Wrong Choice!!!!");
 
-// function to create a stack of given capacity. It initializes size of
-// stack as 0
-struct Stack* createStack(unsigned capacity)
-{
-	struct Stack* stack = (struct Stack*)malloc(sizeof(struct Stack));
-	stack->capacity = capacity;
-	stack->top = -1;
-	stack->array = (int*)malloc(stack->capacity * sizeof(int));
-	return stack;
+       }
+   }
 }
-
-// Stack is full when top is equal to the last index
-int isFull(struct Stack* stack)
-{
-	return stack->top == stack->capacity - 1;
+void createstack(stack1 *sp){
+    sp->top=-1;
 }
-
-// Stack is empty when top is equal to -1
-int isEmpty(struct Stack* stack)
-{
-	return stack->top == -1;
+boolean isempty(stack1 *sp){
+    if(sp->top==-1)
+    return true;
+    else 
+    return false;
 }
-
-// Function to add an item to stack. It increases top by 1
-void push(struct Stack* stack, int item)
-{
-	if (isFull(stack))
-		return;
-	stack->array[++stack->top] = item;
-	printf("%d pushed to stack\n", item);
+boolean isfull(stack1 *sp){
+    if(sp->top==max-1){
+        return true;
+    }
+    else
+    return false;
 }
-
-// Function to remove an item from stack. It decreases top by 1
-int pop(struct Stack* stack)
-{
-	if (isEmpty(stack))
-		return INT_MIN;
-	return stack->array[stack->top--];
+int pop(stack1 *sp){
+    int temp=sp->ar[sp->top];
+    sp->top--;
+    return temp;
 }
-
-// Function to return the top from stack without removing it
-int peek(struct Stack* stack)
-{
-	if (isEmpty(stack))
-		return INT_MIN;
-	return stack->array[stack->top];
+void push(stack1 *sp, int element){
+    sp->top++;
+    sp->ar[sp->top]=element;
 }
-
-// Driver program to test above functions
-int main()
-{
-	struct Stack* stack = createStack(100);
-
-	push(stack, 10);
-	push(stack, 20);
-	push(stack, 30);
-
-	printf("%d popped from stack\n", pop(stack));
-
-	return 0;
+int peek(stack1 *sp){
+    return(sp->ar[sp->top]);
 }
